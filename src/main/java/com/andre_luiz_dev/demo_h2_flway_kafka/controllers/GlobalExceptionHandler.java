e5cbc2b0f3496664a2ae0180b4f6c01f9e5e2e29
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2025-03-25 12:15:49 -0300

    Add a handler for UserNotFoundException
    Introduce a specific handler to return HTTP 422 (Unprocessable Entity)
    for UserNotFoundException. This improves error handling by providing
    clearer and more precise responses for user-related issues.

--- src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.andre_luiz_dev.demo_h2_flway_kafka.controllers;
 
+import com.andre_luiz_dev.demo_h2_flway_kafka.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +14,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("An unexpected error occurred: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body("User not found: " + ex.getMessage());
     }
 }
