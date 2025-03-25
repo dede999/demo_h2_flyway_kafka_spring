5d270ec746a1367d03979cf9b0260255515f950d
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2025-03-25 09:28:24 -0300

    Add global exception handler for generic errors
    Introduced a `GlobalExceptionHandler` class using `@RestControllerAdvice`
    to handle generic exceptions across the application. This ensures
    unexpected errors return a standardized 500 response with an error
    message.

---
+++ src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.andre_luiz_dev.demo_h2_flway_kafka.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // Handle Generic Exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred: " + ex.getMessage());
+    }
+}
