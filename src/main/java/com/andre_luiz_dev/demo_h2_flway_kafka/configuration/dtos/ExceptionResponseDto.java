afb5a0cde1b3df8e3504adf7d97a00d38016382f
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2025-03-25 12:20:36 -0300

    Refactor exception handling to use ExceptionResponseDto.
    Replaced plain string responses with the new `ExceptionResponseDto`
    record in the `GlobalExceptionHandler`. This improves response
    consistency and structure, providing clearer error messages to clients.

---
+++ src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/configuration/dtos/ExceptionResponseDto.java
@@ -0,0 +1,6 @@
+package com.andre_luiz_dev.demo_h2_flway_kafka.configuration.dtos;
+
+public record ExceptionResponseDto(
+        String message,
+        String exception
+) {}
--- src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.andre_luiz_dev.demo_h2_flway_kafka.controllers;
 
+import com.andre_luiz_dev.demo_h2_flway_kafka.configuration.dtos.ExceptionResponseDto;
 import com.andre_luiz_dev.demo_h2_flway_kafka.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     // Handle Generic Exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
+    public ResponseEntity<ExceptionResponseDto> handleGenericException(Exception ex) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("An unexpected error occurred: " + ex.getMessage());
+                .body(new ExceptionResponseDto("An error occurred", ex.getMessage()));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+    public ResponseEntity<ExceptionResponseDto> handleUserNotFoundException(UserNotFoundException ex) {
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body("User not found: " + ex.getMessage());
+                .body(new ExceptionResponseDto("User not found", ex.getMessage()));
     }
 }
