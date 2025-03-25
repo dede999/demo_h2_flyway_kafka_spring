0e810e9c1d2c13d5e04d95281e212813bd523415
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2025-03-25 14:51:02 -0300

    Add beans for PasswordEncoder and AuthenticationManager
    This commit introduces a PasswordEncoder bean using BCryptPasswordEncoder
    and an AuthenticationManager bean. These additions enhance security
    configuration and support authentication handling in the application.

--- src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/configuration/SecurityConfiguration.java
+++ src/main/java/com/andre_luiz_dev/demo_h2_flway_kafka/configuration/SecurityConfiguration.java
@@ -4,10 +4,14 @@ import com.andre_luiz_dev.demo_h2_flway_kafka.services.auth.SecurityFilterServic
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -30,5 +34,15 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
